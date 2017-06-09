9165ee576f19284930d01d00373c00b61e5fda21
LuluBlondet <maria.mlvp@gmail.com>
2017-06-09 07:30:54 -0500

    notacion ec-6

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,38 @@
-$(function(){
+$( _ => {
 
-	$valInput = $('input:text');
-	$containerBotones = $('.container-botones');
+const $valInput = $('input:text');
+const $containerBotones = $('.container-botones');
+
+	$containerBotones.on('click','input',function(e){
+		 
+		 $valEachNumber = $(this).val();
+		 $valInputCurrent = $valInput.val();
+		 $valInputCurrent += $valEachNumber;
+		 $valInput.val($valInputCurrent);
+
+		if($(this).val() === "C"){
+					$valInput.val(" ");
+				}
+
+		if($(this).is('#result')){
+			const a = $valInput.val().substring(0,($valInput.val().indexOf("=")));
+			$valInput.val(eval(a));
+		}
+
+		$(this).addClass('active');
+		$(this).siblings().removeClass();		
+
+		})
+
+});
+		
+
+
+
+/*$(function(){
+
+var $valInput = $('input:text');
+var $containerBotones = $('.container-botones');
 
 	$containerBotones.on('click','input',function(e){
 		 
@@ -23,6 +54,7 @@ $(function(){
 		$(this).siblings().removeClass();		
 
 		})
+*/		
 /*
 	$(document).on('keydown',function(e){
 		console.log(e);
@@ -32,5 +64,5 @@ $(function(){
 	})	
 	*/
  
-})
+
 
